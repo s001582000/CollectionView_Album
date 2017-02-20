232be8993b76e706bfd43aee0c999ca0b4120359
YaXuan Liang <xuan@zoaks.com>
2017-02-20 16:08:25 +0800

    Album 無限循環

--- Album/ViewController.swift
+++ Album/ViewController.swift
@@ -10,24 +10,43 @@ import UIKit
 
 class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
     var arrData = [String]()
+    var bFirstViewDidPost = true
     @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         //註冊cell
         collectionView.register(UINib(nibName: "MyCell", bundle: nil), forCellWithReuseIdentifier: "MyCell")
         //資料
-        for i in 1...4 {
-            arrData.append("\(i)")
-        }
+        changeData()
         //每個cell的大小
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height/2)
         layout.minimumInteritemSpacing = 0
         layout.minimumLineSpacing = 0
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews();
+        if (bFirstViewDidPost) {
+            bFirstViewDidPost = false;
+            collectionView.reloadData()
+            collectionView.contentOffset = CGPoint(x: UIScreen.main.bounds.size.width, y: 0)
+        }
+    }
+    
+    func changeData() {
+        for i in 1...4 {
+            arrData.append("\(i)")
+        }
+        let firstObject = arrData.first!
+        let lastObject = arrData.last!
+        arrData.insert(firstObject, at: arrData.count)
+        arrData.insert(lastObject, at: 0)
     }
     // collectionViewDelegate
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -45,5 +64,13 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
         return cell
     }
     
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        if scrollView.contentOffset.x == 0 {
+            scrollView.scrollRectToVisible(CGRect(x: scrollView.frame.size.width * CGFloat((arrData.count - 2)), y: 0, width: scrollView.frame.size.width, height: scrollView.frame.size.height), animated: false)
+        }else if scrollView.contentOffset.x == scrollView.frame.size.width * CGFloat((arrData.count - 1)){
+            scrollView.scrollRectToVisible(CGRect(x: scrollView.frame.size.width , y: 0, width: scrollView.frame.size.width, height: scrollView.frame.size.height), animated: false)
+        }
+    }
 
 }
